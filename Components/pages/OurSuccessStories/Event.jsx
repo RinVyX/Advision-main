591b2d5d13555e9b6b98de05c8d0781b328f3961
RinVyX <aggoune.lina.learn@gmail.com>
2023-06-22 10:28:16 +0100

    event (our success stories)

--- Components/pages/OurSuccessStories/Event.jsx
+++ Components/pages/OurSuccessStories/Event.jsx
@@ -4,10 +4,50 @@ const Event = ({ subPage }) => {
   return (
     <Content
       className={`w-full  absolute ${
-        subPage === "event" ? "left-0" : "left-[-200%]"
-      } flex justify-center items-start  ease transform duration-500 transition-all w-[95%] md:w-[95%] flex-col font-Signika mx-4 md:mx-10 my-4`}
+        subPage === "event" ? "left-0 opacity-1" : "left-[-100%] opacity-0"
+      } font-Kanit`}
     >
-      <div className="w-full h-[140px] "></div>
+      <div className="container px-6 py-10 mx-auto ">
+        <h1 className="text-2xl font-semibold text-center text-gray-800 capitalize lg:text-3xl dark:text-white">Portfolio of our events</h1>
+
+        <p className="mt-4 text-center text-gray-500 dark:text-gray-300">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum quam voluptatibus
+        </p>
+        <div className="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-12 md:grid-cols-2 xl:grid-cols-3">
+            <div className="overflow-hidden bg-cover rounded-lg cursor-pointer h-96 group 
+                            bg-[url('/images/Affiche-A3-.png')]">
+                <div
+                    className="flex flex-col justify-center w-full h-full px-8 py-4 transition-opacity duration-700 opacity-0 backdrop-blur-sm bg-gray-800/60 group-hover:opacity-100">
+                    <h2 className="mt-4 text-xl font-semibold text-white capitalize">SEER</h2>
+                    <p className="mt-2 text-lg tracking-wider text-blue-400 uppercase ">Website</p>
+                </div>
+            </div>
+
+            <div className="overflow-hidden bg-cover rounded-lg cursor-pointer h-96 group
+                            bg-[url('/images/Affiche-A4.png')]">
+                <div
+                    className="flex flex-col justify-center w-full h-full px-8 py-4 transition-opacity duration-700 opacity-0 backdrop-blur-sm bg-gray-800/60 group-hover:opacity-100">
+                    <h2 className="mt-4 text-xl font-semibold text-white capitalize">Block of Ui kit collections</h2>
+                    <p className="mt-2 text-lg tracking-wider text-blue-400 uppercase ">Ui kit</p>
+                </div>
+            </div>
+
+            <div className="overflow-hidden bg-cover rounded-lg cursor-pointer h-96 group
+                            bg-[url('/images/EIC-fiche_portrait_V5.jpg')]">
+                <div
+                    className="flex flex-col justify-center w-full h-full px-8 py-4 transition-opacity duration-700 opacity-0 backdrop-blur-sm bg-gray-800/60 group-hover:opacity-100">
+                    <h2 className="mt-4 text-xl font-semibold text-white capitalize">Ton’s of mobile mockup</h2>
+                    <p className="mt-2 text-lg tracking-wider text-blue-400 uppercase "
+                       onClick={() => {
+                          setPage("events"),
+                          setSubPage("eic"),
+                          router.push("/Events"),
+                          setShowRightMenu(false);
+                        }}> Mockups</p>
+                </div>
+            </div>
+        </div>
+      </div>
     </Content>
   );
 };
